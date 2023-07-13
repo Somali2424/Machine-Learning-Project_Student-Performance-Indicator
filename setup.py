e4a62ca189c464186f8325c2ad53bf7f38957cd8
Somali <somalisengupta5@gmail.com>
2023-07-13 13:47:30 +0530

    Data tranformation done

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
 name='mlproject',
 version='0.0.1',
-author='Krish',
-author_email='krishnaik06@gmail.com',
+author='Somali',
+author_email='somalisengupta5@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 
